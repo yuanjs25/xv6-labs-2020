4a2e085a82d1f7e9c86274f15a8d8f2e57a4ea4b
20222005260 <20222005260@example.com>
2024-11-12 17:29:35 -0800

    lab9

--- kernel/fs.h
+++ kernel/fs.h
@@ -4,6 +4,8 @@
 
 #define ROOTINO  1   // root i-number
 #define BSIZE 1024  // block size
+#define NSYMLINK 10
+
 
 // Disk layout:
 // [ boot block | super block | log | inode blocks |
@@ -24,9 +26,11 @@ struct superblock {
 
 #define FSMAGIC 0x10203040
 
-#define NDIRECT 12
+#define NDIRECT 11
 #define NINDIRECT (BSIZE / sizeof(uint))
-#define MAXFILE (NDIRECT + NINDIRECT)
+#define NDOUBLYINDIRECT (NINDIRECT * NINDIRECT)     // lab9-1
+#define MAXFILE (NDIRECT + NINDIRECT + NDOUBLYINDIRECT) // lab9-1
+
 
 // On-disk inode structure
 struct dinode {
@@ -35,7 +39,7 @@ struct dinode {
   short minor;          // Minor device number (T_DEVICE only)
   short nlink;          // Number of links to inode in file system
   uint size;            // Size of file (bytes)
-  uint addrs[NDIRECT+1];   // Data block addresses
+  uint addrs[NDIRECT+1+1];   // Data block addresses
 };
 
 // Inodes per block.
